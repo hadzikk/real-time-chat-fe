81e5e2136345207200b93837a58fa87530efb143
hadzikk <hadzikmochammad@gmail.com>
2026-04-25 20:55:10 +0700

    fix: hardcoded the baseURLto connect the backend server

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, 
+    baseURL: import.meta.env.VITE_API_URL || 'https://real-time-chat-4g23gk1hp-hadzik-mochamad-sofyans-projects.vercel.app/api', 
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
