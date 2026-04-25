70f411d9c0b28e29dd445b54e2b05c0c57f80298
hadzikk <hadzikmochammad@gmail.com>
2026-04-25 21:29:03 +0700

    fix: change base url

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://real-time-chat-4g23gk1hp-hadzik-mochamad-sofyans-projects.vercel.app/api', 
+    baseURL: import.meta.env.VITE_API_URL || 'https://real-time-chat-be.vercel.app/api', 
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
