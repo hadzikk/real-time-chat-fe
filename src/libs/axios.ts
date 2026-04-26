c75a8f0454c082a8992a5a5f08c2c6ef195328b9
hadzikk <hadzikmochammad@gmail.com>
2026-04-26 20:40:29 +0700

    fix: hardcode the backend base url due env is not detected using vite

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://real-time-chat-be.vercel.app/api', // later please use vite env
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
